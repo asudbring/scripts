e597919e67469f189da1a23bcb59b825978ce5f2
asudbring <allen@sudbring.com>
2022-12-26 00:07:54 -0800

    added docker commands and install of CNI script

--- powershell/containers/docker/auzre-cni-docker-windows.ps1
+++ powershell/containers/docker/auzre-cni-docker-windows.ps1
@@ -111,3 +111,24 @@ $vm = @{
     VM = $vmConfig
 }
 New-AzVM @vm
+
+Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
+.\install-docker-ce.ps1
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install git
+choco install jq
+
+git clone https://github.com/Azure/azure-container-networking.git
+
+.\Install-CniPlugin.ps1 v1.4.39
+
+Copy-Item -Path "c:\k\azurecni\bin\10-azure.conflist" -Destination "c:\k\azurecni\netconf"
+
+Restart-Computer
+
+.\docker-exec.ps1 testcontainer default mcr.microsoft.com/windows/servercore/iis add
+
+docker exec -it testcontainer powershell
+
